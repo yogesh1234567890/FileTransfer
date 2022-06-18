193a565ee04c225ca53bc4455c5d5f8f7a988739
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2022-06-18 18:28:42 +0545

    websocket connection established

---
+++ connection/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "connection"
+urlpatterns = [
+    # path('', views.index, name='index'),
+    # path('<str:room_name>/', views.room, name='room'),
+    path("file_upload/", views.file_upload, name="file_upload"),
+    path("generate_code/", views.generate_code, name="generate_code"),
+]
--- filetransfer/asgi.py
+++ filetransfer/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import connection.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'filetransfer.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "filetransfer.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(connection.routing.websocket_urlpatterns)
+        ),
+    }
+)
