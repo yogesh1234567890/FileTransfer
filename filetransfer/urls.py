210df13cd5c5512e01e5a8f11f1745b2de168d7e
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2022-08-10 09:43:19 +0545

    login signup losgout  managed

--- filetransfer/urls.py
+++ filetransfer/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("connection.urls"), name="connection"),
+    path("authentication/", include("accounts.urls"), name="accounts"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
